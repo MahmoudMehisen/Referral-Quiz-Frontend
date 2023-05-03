2c17f503109552d4fea1bf978bc2e850e309ac7b
Mahmoud Mehisen <mahmoud.mehisen@gmail.com>
2023-05-03 11:01:43 +0300

    change option model

--- src/app/shared/models/question-option.model.ts
+++ src/app/shared/models/question-option.model.ts
@@ -1,4 +1,4 @@
 export class QuestionOption {
-  constructor(public id: number, public optionText: string) {
+  constructor(public id: number, public optionText: string, public isCorrectAnswer:boolean) {
   }
 }
